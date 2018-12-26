af32b2cab6781f7e080ca920b6c054c376b78dd8
Matt Harnett <harnettmatt@gmail.com>
2018-12-26 13:17:52 -0800

    Created the Item and User structs

--- main.go
+++ main.go
@@ -4,12 +4,25 @@ import (
 	"net/http"
 )
 
-func ping(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("pong"))
+// Item ...
+type Item struct {
+	name, image, category string
+	price                 float32
+}
+
+// User ...
+type User struct {
+	name    string
+	items   []Item
+	balance float32
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("home page"))
 }
 
 func main() {
-	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/", home)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}

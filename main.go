9e78850a07f94ee20108d2e4a3c2d5e885b505a6
Matt Harnett <harnettmatt@gmail.com>
2018-12-26 13:19:23 -0800

    placeholders for the items and users pages

--- main.go
+++ main.go
@@ -21,8 +21,18 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("home page"))
 }
 
+func users(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("users page"))
+}
+
+func items(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("items page"))
+}
+
 func main() {
 	http.HandleFunc("/", home)
+	http.HandleFunc("/users", users)
+	http.HandleFunc("/items", items)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
